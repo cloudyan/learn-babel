7a39b4a40a23a251e171c603a220c37c85584118
yanzhanjun <yanzhanjun@lattebank.com>
2020-12-31 15:16:46 +0800

    fix: babelrc browsers可以配置为数组

--- babel-7/demo-in-webpack/babel.config.js
+++ babel-7/demo-in-webpack/babel.config.js
@@ -11,6 +11,12 @@ module.exports = function(api) {
         corejs: 3,
         // include: ['es6.map'],
         // targets: 'defaults', // 此处会和 .browserslistrc 配置合并处理
+        targets: {
+          browsers: [ // 也可以用数组 也可以应逗号隔开的字符串
+            "ie >= 9",
+            "chrome 60"
+          ]
+        },
       }
     ]
   ]
